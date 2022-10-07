303afa528f0b70ee6e09f46733e268e3a2c6b130
ashvntha <ashvntha@ashvnthas-MacBook-Pro.local>
2022-10-07 16:22:41 +0200

    add custom colors and font tailwind config

--- layout/header.tsx
+++ layout/header.tsx
@@ -6,16 +6,16 @@ import { UserCircleIcon } from "@heroicons/react/24/outline";
 type headerProps = {};
 
 export default function Header({}: headerProps) {
-  return (
-    <div className="h-auto top-0 p-2">
-      <div className="flex justify-between">
-        <Link href="/">
-          <HomeIcon className="h-10 w-10" />
-        </Link>
-        <Link href="/avatar">
-          <UserCircleIcon className="h-10 w-10" />
-        </Link>
-      </div>
-    </div>
-  );
+	return (
+		<div className="h-auto top-0 p-2">
+			<div className="flex justify-between">
+				<Link href="/">
+					<HomeIcon className="h-10 w-10" />
+				</Link>
+				<Link href="/avatar">
+					<UserCircleIcon className="h-10 w-10" />
+				</Link>
+			</div>
+		</div>
+	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./layout/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+		"./layout/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		fontFamily: {
+			sans: ["Lato", "sans-serif"],
+		},
+		extend: {
+			darkblue: "#064789",
+			lightblue: "#BCD4DE",
+			blue: "#586BA4",
+			yellow: "#F5DD90",
+			orange: "#F68E5F",
+			red: "#F76C5E",
+			darksage: "#7A9BA9",
+			buttongrey: "#F3F3F3",
+		},
+	},
+	plugins: [],
 };
