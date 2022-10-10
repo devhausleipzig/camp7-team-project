f2cdaf3ada3ec27eb9d0507455f0df1b276489bd
mustafa <mustafa.naouf@outlook.com>
2022-10-10 18:05:40 +0200

    need to be beleted 2

--- components/taskCard.tsx
+++ components/taskCard.tsx
@@ -7,7 +7,7 @@ export default function TaskCard({}: taskCardProps) {
     <div className="flex flex-col bg-black rounded-md h-[29%] w-[88%] shadow-md">
       <div className="flex ">1</div>
       <div className="flex">1</div>
-      <div className="flex">1</div>
+      <div className="flex">3</div>
     </div>
   );
 }
