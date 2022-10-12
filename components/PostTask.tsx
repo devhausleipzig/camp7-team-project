36d43a0394eaada8173ab09a93d3436cb13955c6
Chirag Singal <chirag.prakriya@gmail.com>
2022-10-12 09:13:02 +0200

    made changes in the code for the add task

--- components/PostTask.tsx
+++ components/PostTask.tsx
@@ -14,13 +14,14 @@ interface Props {
 
 export function TaskForm({ onSubmit, task, updateField, buttonText }: Props) {
 	return (
-		<div className="flex flex-col text-blue-700 items-center h-screen ">
+		<div className="flex flex-col text-custom_darkblue font-bold items-center h-screen">
 			<form
 				onSubmit={onSubmit}
 				className="flex flex-col gap-2 max-w-4xl mx-auto"
 			>
 				<label htmlFor="task-title">Task</label>
 				<input
+					className="border border-neutral-50 border-b-custom_darkblue  text-black"
 					type="text"
 					id="task-title"
 					value={task.title}
@@ -29,16 +30,18 @@ export function TaskForm({ onSubmit, task, updateField, buttonText }: Props) {
 				/>
 				<label htmlFor="points">Points</label>
 				<input
+					className="border text-black border-neutral-50 border-b-custom_darkblue"
 					type="number"
 					id="points"
 					value={task.points}
 					placeholder="enter a number here"
 					onChange={(event) => updateField(event, "points")}
 				/>
-				<div className="flex">
+				<div className="flex gap-2">
 					<div className="flex flex-col">
 						<label htmlFor="time">End Time</label>
 						<input
+							className="border text-black border-neutral-50 border-b-custom_darkblue"
 							type="time"
 							id="time"
 							value={task.endTime}
@@ -49,6 +52,7 @@ export function TaskForm({ onSubmit, task, updateField, buttonText }: Props) {
 					<div className="flex flex-col">
 						<label htmlFor="date">End Date</label>
 						<input
+							className="border text-black border-neutral-50 border-b-custom_darkblue"
 							type="date"
 							id="date"
 							value={task.endDate}
@@ -59,6 +63,7 @@ export function TaskForm({ onSubmit, task, updateField, buttonText }: Props) {
 				</div>
 				<label htmlFor="note">Note</label>
 				<textarea
+					className="border text-black border-neutral-50 border-b-custom_darkblue"
 					rows={2}
 					id="note"
 					placeholder="Input note here..."
