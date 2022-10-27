ceb28ca93c88eedc23352b8b717eb5ee99c57f83
Chirag Singal <chirag.prakriya@gmail.com>
2022-10-27 11:41:54 +0200

    incomplete and may Mustafa save the day from here

--- pages/api/task/index.ts
+++ pages/api/task/index.ts
@@ -2,16 +2,20 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { format, addDays } from "date-fns";
 import { prisma } from "../../../prisma/db";
 import { methods } from "../../../utils/methods";
+import user from "../user";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 	try {
 		if (req.method == methods.get) {
 			const tasks = await prisma.task.findMany({
 				where: {
-					completed: false
+					completed: false,
+				},
+				include: {
+					assignedTo: true,
 				},
 				take: 3,
-				orderBy: [{ endDate: "asc" }, { endTime: "asc" }]
+				orderBy: [{ endDate: "asc" }, { endTime: "asc" }],
 			});
 
 			res.status(200).json(tasks);
@@ -26,10 +30,13 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 					points: Number(taskData.points),
 					createdBy: {
 						connect: {
-							id: creatorId
-						}
-					}
-				}
+							id: creatorId,
+						},
+					},
+					assignedTo: {
+						connect: taskData.assignedTo.map((user)=>),
+					},
+				},
 			});
 
 			res.status(201).json({ id: task.id });
