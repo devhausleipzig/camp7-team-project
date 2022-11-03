f9bfc51fc147904282b995342451df4a797123fb
Franz Wollang <franzwollang@gmail.com>
2022-11-03 14:36:04 +0100

    Prep for querying user's points from DB

---
+++ pages/api/user/points.ts
@@ -0,0 +1,26 @@
+import { Sql } from "@prisma/client/runtime";
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { prisma } from "../../../prisma/db";
+import { methods } from "../../../utils/methods";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+	try {
+		if (req.method == methods.get) {
+			// const users = await prisma.$queryRaw`
+			//     SELECT SUM("task".points) as userPoints
+			//     FROM "user"
+			//     JOIN "userOnTask" ON "user".id = "userOnTask".userId
+			//     JOIN "task" ON "userOnTask".taskId = "task.id"
+			//     GROUP BY "user".id
+			// `
+			res.status(200).end();
+			return;
+		}
+
+		res.status(500).send("unknown request");
+	} catch (err) {
+		console.log(err);
+		res.status(500).end();
+	}
+};
